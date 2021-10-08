5c3dbddb47f1d2204125a6d696992dccc853716f
imshahastam <shahzada.stamova@gmail.com>
2021-10-08 14:03:36 +0600

    first version

--- src/Calculator.kt
+++ src/Calculator.kt
@@ -1,2 +1,20 @@
-class Calculator {
+fun main() {
+    var i1: Double
+    var i2: Double
+    var ff: String
+
+    println("Enter first number: ")
+    i1 = readLine()!!.toDouble()
+    println("Enter second number: ")
+    i2 = readLine()!!.toDouble()
+
+    println("What operation?")
+    ff = readLine().toString()
+
+    when (ff) {
+        "+" -> print("Сложение ${i1 + i2}")
+        "-" -> print("Вычитание ${i1-i2}")
+        "*" -> print("Умножение ${i1*i2}")
+        "/" -> print("Деление ${i1/i2}")
+    }
 }
